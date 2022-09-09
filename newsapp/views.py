5fcf37075764098533ee8d9a57875b1740ccc2fa
Amr Atif <62966382+amr-atif@users.noreply.github.com>
2022-09-10 00:48:40 +0200

    signup form done

--- newsapp/views.py
+++ newsapp/views.py
@@ -1,5 +1,7 @@
 from django.views.generic import ListView, DetailView
 
+from doctors.models import Doctor
+
 from .models import News
 
 
@@ -12,3 +14,13 @@ class NewPageView(ListView):
 class NewDetailView(DetailView):
     model = News
     template_name = 'newsapp/new_detail.html'
+
+
+class IsDoctor(ListView):
+    model = Doctor
+    context_object_name = 'doctors'
+
+    def get_queryset(self):
+        doctor_query = Doctor.objects.all()
+        return doctor_query
+    template_name = 'newsapp/news_list.html'
